edfb5c494b62475cffb448b4557556b48dcc56f2
rcatullo <rcatullo@stanford.edu>
2024-11-14 20:54:36 -0800

    Update page.tsx
    Try to fix list item.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,8 +3,17 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
       <h2 className="text-xl mb-4">Papers</h2>
-      <ul class="list-disc font-serif">
-        <li>“PSPACE-hardness of Two-Player Games”, with Tanvi Deshpande, 2024, Stanford University, available <a href="https://obnh745pyfptrm8z.public.blob.vercel-storage.com/papers/PSPACE-hardness%20of%20Two-Player%20Games.pdf" download="PSPACE-hardness of Two-Player Games"></a>here.</li>
+      <ul className="list-disc">
+        <li className="font-serif">
+          “PSPACE-hardness of Two-Player Games”, with Tanvi Deshpande, 2024, Stanford University, available 
+            <a className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://obnh745pyfptrm8z.public.blob.vercel-storage.com/papers/PSPACE-hardness%20of%20Two-Player%20Games.pdf"
+              download>
+            here
+            </a>.
+          </li>
       </ul>
     </section>
   )
