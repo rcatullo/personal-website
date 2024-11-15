36eb889b8afce9362a8580e6b4791a68435c5546
rcatullo <rcatullo@stanford.edu>
2024-11-15 02:49:33 -0800

    attempt underline animation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,17 +5,18 @@ export default function Page() {
       <h2 className="text-xl mb-4">Papers</h2>
       <ul className="list-disc space-y-4">
         <li className="font-serif">
-          <p className="font-semibold">“PSPACE-hardness of Two-Player Games”</p>, with Tanvi Deshpande, 2024, Stanford University, available 
-            <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+          <p className="transition-all font-semibold">“PSPACE-hardness of Two-Player Games”</p>, with Tanvi Deshpande, 2024, Stanford University, available 
+            <a className="transition-all group duration-300"
               rel="noopener noreferrer"
               target="_blank"
               href="https://obnh745pyfptrm8z.public.blob.vercel-storage.com/papers/PSPACE-hardness%20of%20Two-Player%20Games.pdf"
               download>
              {` here`}
+             <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
             </a>.
           </li>
           <li className="font-serif">
-          <p className="font-semibold">“Riemann's Existence Theorem and Admissible Covers"</p>, 2024, under the supervision of Mohan Swaminathan, Stanford 
+          <p className="transition-all font-semibold">“Riemann's Existence Theorem and Admissible Covers"</p>, 2024, under the supervision of Mohan Swaminathan, Stanford 
           Undergraduate Research Institute in Mathematics, available 
             <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
               rel="noopener noreferrer"
@@ -26,7 +27,7 @@ export default function Page() {
             </a>.
           </li>
           <li className="font-serif">
-          <p className="font-semibold">"Supersingular Diagonal Curves and their Genera"</p>, with A. Sukhadia and M. Machado, 2023, Stanford 
+          <p className="transition-all font-semibold">"Supersingular Diagonal Curves and their Genera"</p>, with A. Sukhadia and M. Machado, 2023, Stanford 
           Undergraduate Research Institute in Mathematics, available 
             <a className="transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
               rel="noopener noreferrer"
