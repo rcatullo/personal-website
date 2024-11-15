d27582bd38139b7decc8ff11c5221b3f31b3c71a
rcatullo <rcatullo@stanford.edu>
2024-11-14 19:04:47 -0800

    Update page.tsx
    Add papers section

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
+      <h2 className="text-xl mb-4">Papers</h2>
     </section>
   )
 }
