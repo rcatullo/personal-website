153a8c49dfa05ef14f80ea00826fa61d1bfecc03
rcatullo <rcatullo@stanford.edu>
2024-11-14 20:46:56 -0800

    Update page.tsx
    Add two-player paper

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,9 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Projects</h1>
       <h2 className="text-xl mb-4">Papers</h2>
+      <ul class="list-disc font-serif">
+        <li>“PSPACE-hardness of Two-Player Games”, with Tanvi Deshpande, 2024, Stanford University, available <a href="https://obnh745pyfptrm8z.public.blob.vercel-storage.com/papers/PSPACE-hardness%20of%20Two-Player%20Games.pdf" download="PSPACE-hardness of Two-Player Games"></a>here.</li>
+      </ul>
     </section>
   )
 }
