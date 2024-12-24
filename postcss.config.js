e01d659dc502b1885e377b97526e1b01a9115bc7
Ryan Catullo <rcatullo@stanford.edu>
2024-12-24 02:31:03 -0500

    Fix postcss.config.js and import from @vercel/og instead of next/og.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  plugins: [
-    require('tailwindcss')('./tailwind.config.js'),
-    require('autoprefixer'),
-    require('tailwindcss-border-gradients')()
-  ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
