7d741faff4d8c5adede444db6abc5c20dfa9d6d2
Ryan Catullo <rcatullo@stanford.edu>
2025-05-26 16:31:07 -0700

    fix: private admin email

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -14,7 +14,7 @@ const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async signIn({ account, profile }) {
-      return profile.email === "rcatullo13@gmail.com"
+      return profile.email === process.env.ADMIN_EMAIL
     }
   }
 });
