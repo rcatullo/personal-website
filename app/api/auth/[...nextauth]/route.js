769b43e7e0c26c8cadcb2694e717d407e94cedc6
Ryan Catullo <rcatullo@stanford.edu>
2025-05-26 16:30:15 -0700

    feat: rough email verification

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,22 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-
 const handler = NextAuth({
-providers: [
+  providers: [
     GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-],
-secret: process.env.NEXT_PUBLIC_SECRET
+  ],
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ account, profile }) {
+      return profile.email === "rcatullo13@gmail.com"
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
