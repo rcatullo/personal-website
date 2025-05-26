ab561bf94f9e5677abf0ddbe5b1600d38cf9e9a7
Ryan Catullo <rcatullo@stanford.edu>
2025-05-26 16:37:09 -0700

    refactor: move nextauth-session-provider to auth folder.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
-import AuthProvider from "./components/nextauth-session-provider";
+import AuthProvider from "./components/auth/nextauth-session-provider";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
